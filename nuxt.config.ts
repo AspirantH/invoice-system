f24fbc8951c5e956d91ad49d7410fda55e5f6b2d
AspirantH <93640658+AspirantH@users.noreply.github.com>
2026-03-02 22:17:14 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: false,
-  devServer: port: 3004 },
+  devServer: {
+    port: 3004,
+  },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
-  typescript: { strict: true, typeCheck: false },
-  experimental: { appManifest: false },
+  typescript: {
+    strict: true,
+    typeCheck: false
+  },
+  experimental: {
+    appManifest: false
+  },
   app: {
     head: {
       title: '个人报销管理系统',
